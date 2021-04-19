b47f15fdbffafa7b37ee775eb256bc231edfd541
arthur2001 <ar.2001.thur@outlook.com>
2021-04-19 02:44:49 -0700

    Update opencv_object_tracking.py

--- opencv_object_tracking.py
+++ opencv_object_tracking.py
@@ -10,31 +10,22 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import rainflow
+from scipy.interpolate import UnivariateSpline
+from scipy.signal import wiener, filtfilt, butter, gaussian, freqz
+from scipy.ndimage import filters
+import scipy.optimize as op
 import pandas as pd
-from cydets.algorithm import detect_cycles
+from scipy import fftpack
+from scipy import signal
+from scipy import optimize
 import ctypes
-import os
-
-def Average(lst):
-    return sum(lst) / len(lst)
 
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
-def get_cycle(change_val):
-    series = pd.Series(change_val)
-    cycles = detect_cycles(series)
-    arr = cycles['duration'];    
-    
-    max = arr[0]
-    #Loop through the array    
-    for i in range(0, len(arr)):    
-        #Compare elements of array with max    
-       if(arr[i] > max):    
-           max = arr[i]
-           
-    return max
-            
-    
+def round_half_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.floor(n*multiplier + 0.5) / multiplier
 
 class Plotter:
     def __init__(self, plot_width, plot_height,sample_buffer=None):
@@ -59,9 +50,22 @@ class Plotter:
             self.plot_t_last[label] = 0
             
         self.plots[label].append(int(val))
+        
         while len(self.plots[label]) > self.sample_buffer:
             self.plots[label].pop(0)
-            self.show_plot(label)    
+            self.show_plot(label)
+   
+    def plotarray(self, val, label = "plot"):
+        if not label in self.plots:
+            self.plots[label] = []
+            self.plot_t_last[label] = 0
+        for i in val :    
+            self.plots[label].append(int(i))
+        
+        while len(self.plots[label]) > self.sample_buffer:
+            self.plots[label].pop(0)
+            self.show_plot(label)        
+
     # Show plot using opencv imshow
     def show_plot(self, label):
 
@@ -89,6 +93,82 @@ class Plotter:
         cv2.imshow(label, self.plot_canvas)
         cv2.waitKey(1)
 
+def parse_ICA_results(ICA, buffer_window): #time
+	
+
+	# ** FOR RGB CHANNELS & ICA **
+	#one = np.squeeze(np.asarray(ICA[:, 0])).tolist()
+	
+	
+	#one = (np.hamming(len(one)) * one)
+	
+
+	# print "one: ", one.astype(float).tolist()
+	
+
+	#one = np.absolute(np.square(np.fft.irfft(one))).astype(float).tolist()
+	
+
+	#power_ratio = [0, 0, 0]
+	#power_ratio[0] = np.sum(one)/np.amax(one)
+	
+
+	
+	#signals["array"] = one
+	
+	# print power_ratio
+	# print signals
+	#return signals
+
+	# # ** FOR GREEN CHANNEL ONLY **
+	hamming = (np.hamming(len(ICA)) * ICA)
+	fft = np.fft.rfft(hamming)
+	fft = np.absolute(np.square(fft))
+	signals = fft.astype(float).tolist()
+
+	return signals
+
+
+	# ** experiments **
+	# ** for interpolation and hamming **
+	# even_times = np.linspace(time[0], time[-1], len(time))
+	# interpolated_two = np.interp(even_times, time, np.squeeze(np.asarray(ICA[:, 1])).tolist())
+	# interpolated_two = np.hamming(len(time)) * interpolated_two
+	# interpolated_two = interpolated_two - np.mean(interpolated_two)
+
+	# signals["two"] = interpolated_two.tolist()
+
+	# #fft = np.fft.rfft(np.squeeze(np.asarray(ICA[:, 1])))
+	# #signals["two"] = fft.astype(float).tolist()
+
+
+def normalize_matrix(matrix):
+	# ** for matrix
+	for array in matrix:
+		average_of_array = np.mean(array)
+		std_dev = np.std(array)
+
+		for i in range(len(array)):
+			array[i] = ((array[i] - average_of_array)/std_dev)
+	return matrix
+
+def normalize_array(array):
+	#** for array
+	average_of_array = np.mean(array)
+	std_dev = np.std(array)
+
+	for i in range(len(array)):
+		array[i] = ((array[i] - average_of_array)/std_dev)
+	return array
+
+def butter_lowpass_filter(data, cutoff, fs, order):
+    print("Cutoff freq " + str(cutoff))
+    nyq = 0.5 * fs # Nyquist Frequency
+    normal_cutoff = cutoff / nyq
+    # Get the filter coefficients 
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    y = filtfilt(b, a,data)
+    return y
 
 def get_similarity(image1, image2):
     """This function returns the absolute
@@ -98,6 +178,7 @@ def get_similarity(image1, image2):
     gray_image = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     histogram = cv2.calcHist([gray_image], [0], 
                          None, [256], [0, 256])
+
     gray_image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
     histogram2 = cv2.calcHist([gray_image2], [0], 
                           None, [256], [0, 256])
@@ -109,7 +190,11 @@ def get_similarity(image1, image2):
     
     return nsumFrameColor
 
+def func(x, a, b, c):
+    return a * np.exp(-b * x) + c
 
+def test_func(x, a, b,c):
+    return a * np.sin(b * x)+c
 
 def get_frame_value(image, width, height):
     """This function returns the absolute
@@ -125,8 +210,80 @@ def get_frame_value(image, width, height):
 def plotScatter(plt, x, y):
     plt.scatter(x, y);
     plt.pause(0.05)
-    
 
+def filterFreq(fftArray, freqs, framerate):
+    filteredFFT = list()
+    filteredFreqBin = list()
+
+    for i in range(len(fftArray)):
+        if (fftArray[i] > 0.80) and (fftArray[i] < 3):
+            
+            filteredFFT.append(freqs[i]);
+            filteredFreqBin.append((fftArray[i])/1);
+		
+    for i in range(len(filteredFFT)):
+        filteredFFT[i] = abs(i) ** 2
+    normalizedFreqs = filteredFFT
+
+    idx = normalizedFreqs.index(min(normalizedFreqs))
+    freq_in_hertz = filteredFreqBin[idx];
+	
+    freqs = {'normalizedFreqs': normalizedFreqs, 'filteredFreqBin': filteredFreqBin, 'freq_in_hertz': freq_in_hertz}
+    return freqs
+
+def frequencyExtract(spectrum, fps_video):
+    
+    val = None
+    n = None 
+    d = None
+    N = None
+    p1 = list()
+    p2 = list()
+    results = list()
+    freqs = list()
+
+    #from numpy.fft.fftfreq doc: "Sample spacing (inverse of the sampling rate). Defaults to 1. For instance, if the sample spacing is in seconds, then the frequency unit is cycles/second."
+   	# if my frequency is 15 fps, or 15 Hz, or every 66ms, then the sample spacing is .066 seconds (1/15)
+   
+    n = len(spectrum);
+    d = 1.0/fps_video;
+   
+    val = 1.0/(n * d);
+    N = int(((n - 1)/2 + 1))
+    for i in range(N):
+        p1.append(i)
+    i = int(-(n/2))
+    while i<0:
+        
+        p2.append(i)
+        i += 1
+        
+    for i in p2 :
+        p1.append(p2[i])
+    
+    results = p1
+    for i in range(len(results)):
+        p1[i] = i * val
+    freqs = p1
+    return filterFreq(spectrum, freqs, fps_video)
+
+def findPeak(signal_arr):
+    dary = signal_arr
+    dary -= np.average(dary)
+
+    step = np.hstack((np.ones(len(dary)), -1*np.ones(len(dary))))
+
+    dary_step = np.convolve(dary, step, mode='valid')
+
+    # Get the peaks of the convolution
+    peaks = signal.find_peaks(dary_step, width=30)[0]
+    return peaks
+#plt.ion()  ## Note this correction
+#fig = plt.figure()
+
+#plt.title("Real Time plot")
+#plt.xlabel("frame")
+#plt.ylabel("result")
 
 xlist = list()
 ylist = list()
@@ -182,20 +339,17 @@ else:
 fps = None
 fps_video = vs.get(cv2.CAP_PROP_FPS)
 
-total_frame_count = int(vs.get(cv2.CAP_PROP_FRAME_COUNT))
-file_len_sec = float(total_frame_count/fps_video)
-
 index = 0
+indexFrm = 0
 v = 0
 # loop over frames from the video stream
-p = Plotter(400, 200,sample_buffer = 200)
-
-first_Image = None
-change_val = list()
-cycle_value = None
-cycle_value_second = list()
-cycles_frame = fps_video * 4
-
+p = Plotter(400, 200,sample_buffer=200)
+first_Image = None;
+maxcl = 0
+pplotpix = []
+index_per_cycle = 0
+rate_per_ten = list()
+heart_rate_realtime = list()
 while True:
     # grab the current frame, then handle if we are using a
     # VideoStream or VideoCapture object
@@ -231,23 +385,57 @@ while True:
             fileName = "cropped_" + str(index) + "_.jpg"
             index += 1
             cv2.imshow("cropped field", crop_img)
-            cl = get_similarity(first_Image,crop_img)
-            change_val.append(cl)
-            length = len(change_val)
-
-            if(length ==  cycles_frame):
-                cycle_value = get_cycle(change_val)
+            #p.plotarray(y_data,label='result')
+            
+            if index < fps_video * 3 :
+                gray_image = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY);
+                Binary = cv2.adaptiveThreshold(gray_image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                               cv2.THRESH_BINARY,11,2)
+                cl = np.average(Binary)
                 
-                change_val = list()
-                length = 0
-                if(cycle_value != None):
+                if cl > maxcl:
                     
-                    heartrate_sec =float( cycle_value / fps_video)
-                    heartrate_min = int(60/heartrate_sec)
-                    cycle_value_second.append(heartrate_min)
-            
-            p.plot(cl,label='Real-time Result')
-            
+                    maxcl = cl
+                    first_Image = crop_img
+                    text = "{}: {}".format(index+1, cl)
+                    cv2.putText(crop_img, text,( 10,  20),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+                    fileName = str(index+1)+"_train.png"
+                    #cv2.imwrite(fileName, crop_img)
+            else:
+                
+                cl = get_similarity(first_Image,crop_img);
+                pplotpix.append(cl);
+                p.plot(cl,label='result')
+                indexFrm += 1
+                
+                if indexFrm >= fps_video * 7 and indexFrm % fps_video == 0:
+                    y_data = np.array(pplotpix)
+                    x_data = np.arange(0,len(y_data));
+                    y_data = normalize_array(y_data[:,0])
+                    
+                    #
+                    s = UnivariateSpline(x_data, y_data, s=50)
+                    yy_data = s(x_data)
+                    
+                    peaks = findPeak(yy_data);
+                    
+                    cycle = 0
+                    heatrate = 0
+                    if(len(peaks) > 1):
+                        for ii in range(len(peaks)):
+                            if ii < len(peaks)-1 :
+                                cycle += (peaks[ii+1]-peaks[ii])
+                        cycle  = cycle/(len(peaks)-1)
+                        cycle = cycle/fps_video
+                        heatrate = 60/ cycle
+                        heatrate *= 6
+                    index_per_cycle += 1
+                    if index_per_cycle % 10 == 0:
+                        rate_per_ten.append(heatrate)
+                    heart_rate_realtime.append(heatrate)
+                    
+                    print("Heart rate=>"+str(heatrate))
             #cv2.imwrite(fileName, crop_img)
         area = (w - x) * (h - y)
 
@@ -257,10 +445,7 @@ while True:
         frameno += 1;
         xlist.append(frameno);
         ylist.append(area);
-        # plt.plot(xlist,ylist,'-o');
-        # plt.show()
-        # plt.pause(0.0001)
-        # update the FPS counter
+        
 
         fps.update()
 
@@ -306,6 +491,50 @@ while True:
     elif key == ord("q"):
         break
 
+y_data = np.array(pplotpix)
+x_data = np.arange(0,len(y_data));
+y_data = normalize_array(y_data[:,0])
+
+#
+s = UnivariateSpline(x_data, y_data, s=50)
+yy_data = s(x_data)
+
+peaks = findPeak(yy_data);
+
+cycle = 0
+heatrate = 0
+if(len(peaks) > 1):
+    for ii in range(len(peaks)):
+        if ii < len(peaks)-1 :
+            cycle += (peaks[ii+1]-peaks[ii])
+    cycle  = cycle/(len(peaks)-1)
+    cycle = cycle/fps_video
+    heatrate = 60/ cycle
+
+print("total_Heart rate=>"+str(heatrate))
+heatrate = round_half_up(heatrate, 0)
+heatrate *= 6
+
+Mbox("Measure result", "Total heartbeat of this animal is " + str(heatrate), 0)
+# plots
+plt.figure()
+
+plt.plot(y_data)
+
+#plt.plot(dary_step/10)
+
+for ii in range(len(peaks)):
+    plt.plot((peaks[ii], peaks[ii]), (-3, 3), 'r')
+
+plt.show()
+
+
+data_save = {'Heartbeat every 10 seconds': rate_per_ten, 'Average heart rate(/min)': heatrate}
+df_save = pd.DataFrame(data = data_save)
+df_save.to_csv('result_bps.csv')
+
+Mbox("Success message", "Result saved to 'result_bps.csv' successfully!", 0)
+
 # if we are using a webcam, release the pointer
 if not args.get("video", False):
     vs.stop()
@@ -314,40 +543,5 @@ if not args.get("video", False):
 else:
     vs.release()
 
-
-optimize_size = len(cycle_value_second)
-opt_list = list()
-opt_list = cycle_value_second[1:]
-
-space_time = float(file_len_sec/(optimize_size - 1))
-
-x = np.arange(0, file_len_sec, float(file_len_sec/ len(opt_list) ))
-#print(len(x))
-time_list = list()
-
-for i in range(len(opt_list)):
-    val_insert = str(int(i * space_time )) + " s" + " - " + str(int((i+1) * space_time )) + " s"
-    time_list.append(val_insert)
-
-
-fig = plt.figure()
-# Instead of set_figwidth(30)
-fig.set_size_inches(fig.get_figwidth(), 2, forward=True)
-
-plt.title("BPS graph")
-plt.xlabel("Time Line (s)")
-plt.ylabel("B P S")
-#plt.plot(x, opt_list)
-#print(len(opt_list))
-#plt.show()
-
-average_rate = Average(opt_list)
-
-data_save = {'From-To': time_list, 'Heart rate': opt_list, 'Average heart rate(/min)': average_rate}
-df_save = pd.DataFrame(data = data_save)
-df_save.to_csv('result_bps.csv')
-
-Mbox("Success message", "Result saved to 'result_bps.csv' successfully!", 0)
-#os.system("Excel.exe" + 'result_bps.csv')
 # close all windows
-#cv2.destroyAllWindows()
+cv2.destroyAllWindows()
